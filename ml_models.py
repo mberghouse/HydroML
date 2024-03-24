eb39b01223168c51e7b7a10ac87a30a15e765895
mberghouse <55556564+mberghouse@users.noreply.github.com>
2024-03-23 17:14:37 -0700

    More models

--- ml_models.py
+++ ml_models.py
@@ -3,9 +3,8 @@
 
 from sklearn.linear_model import LogisticRegression, LinearRegression, Ridge, RidgeCV, Lasso, LassoLars, BayesianRidge, TweedieRegressor, SGDRegressor, SGDClassifier, Perceptron, TheilSenRegressor, HuberRegressor, ElasticNet, OrthogonalMatchingPursuit
 from sklearn.cluster import KMeans, AgglomerativeClustering, Birch, DBSCAN, BisectingKMeans, MiniBatchKMeans, SpectralClustering
-from sklearn.cluster import affinity_propagation, cluster_optics_dbscan, cluster_optics_xi, dbscan, estimate_bandwidth, k_means, kmeans_plusplus, mean_shift, spectral_clustering, ward_tree
-from sklearn.compose import ColumnTransformer
-from sklearn.covariance import EmpiricalCovariance, GraphicalLasso, GraphicalLassoCV, MinCovDet, OAS, empirical_covariance, graphical_lasso, ledoit_wolf, oas
+#from sklearn.cluster import affinity_propagation, cluster_optics_dbscan, cluster_optics_xi, dbscan, estimate_bandwidth, k_means, kmeans_plusplus, mean_shift, spectral_clustering, ward_tree
+#from sklearn.covariance import EmpiricalCovariance, GraphicalLasso, GraphicalLassoCV, MinCovDet, OAS, empirical_covariance, graphical_lasso, ledoit_wolf, oas
 from sklearn.cross_decomposition import CCA, PLSCanonical, PLSRegression, PLSSVD
 from sklearn.svm import SVC, SVR
 from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
@@ -18,15 +17,44 @@ from sklearn.ensemble import BaggingClassifier, BaggingRegressor, ExtraTreesClas
 
 class ModelSelector:
     def __init__(self):
+    
+        self.decomp_models = {
+        "CCA":CCA,
+        "PLSCanonical":PLSCanonical,
+        "PLSRegression":PLSRegression,
+        "PLSSVD":PLSSVD,
+        "FactorAnalysis":FactorAnalysis,
+        "FastICA":FastICA,
+        "IncrementalPCA":IncrementalPCA,
+        "MiniBatchSparsePCA":MiniBatchSparsePCA,  
+        "PCA":PCA
+        }     
+        
+        self.cluster_models = {
+        "KMeans": KMeans,
+        "AgglomerativeClustering": AgglomerativeClustering,
+        "Birch": Birch,
+        "DBSCAN": DBSCAN,
+        "BisectingKMeans": BisectingKMeans,
+        "SpectralClustering": SpectralClustering,
+        "MiniBatchKMeans": MiniBatchKMeans
+        }
+        
         self.regression_models = {
+        "BaggingRegressor": BaggingRegressor,
+        "ExtraTreesRegressor":ExtraTreesRegressor,
+        "GradientBoostingRegressor": GradientBoostingRegressor,
+        "StackingRegressor": StackingRegressor,
+        "HistGradientBoostingRegressor": HistGradientBoostingRegressor,
+        "VotingRegressor": VotingRegressor,
+        "MLPRegressor": MLPRegressor,
         "Ridge": Ridge, 
         "RidgeCV": RidgeCV, 
         "Lasso": Lasso, 
         "LassoLars": LassoLars,
         "BayesianRidge": BayesianRidge, 
         "TweedieRegressor": TweedieRegressor, 
-        "SGDRegressor": SGDRegressor, 
-        "SGDClassifier": SGDClassifier, 
+        "SGDRegressor": SGDRegressor,        
         "Perceptron": Perceptron, 
         "TheilSenRegressor": TheilSenRegressor,
         "HuberRegressor": HuberRegressor,
@@ -38,13 +66,349 @@ class ModelSelector:
             "KNeighborsRegressor": KNeighborsRegressor,
             "XGBRegressor": xgbr
         }
+        
         self.classification_models = {
+            "BernoulliNB": BernoulliNB,
+            "MLPClassifier": MLPClassifier,
+            "BernoulliRBM": BernoulliRBM,
+            "CategoricalNB":CategoricalNB,
+            "ComplementNB": ComplementNB,
+            "GaussianNB": GaussianNB,
+            "MultinomialNB": MultinomialNB,
+            "BaggingClassifier": BaggingClassifier,
+            "ExtraTreesClassifier": ExtraTreesClassifier,
+            "GradientBoostingClassifier": GradientBoostingClassifier,
+            "StackingClassifier": StackingClassifier,
+            "VotingClassifier": VotingClassifier,
+            "HistGradientBoostingClassifier": HistGradientBoostingClassifier,
             "RandomForestClassifier": RandomForestClassifier,
             "LogisticRegression": LogisticRegression,
             "SVC": SVC,
-            "KNeighborsClassifier": KNeighborsClassifier
+            "KNeighborsClassifier": KNeighborsClassifier,
+            "SGDClassifier": SGDClassifier
         }
+        
         self.model_parameters = {
+        "CCA": {
+            "n_components": 2,
+            "scale": True,
+            "max_iter": 500,
+            "tol": 1e-06,
+            "copy": True
+        },
+        "PLSCanonical": {
+            "n_components": 2,
+            "scale": True,
+            "algorithm": "nipals",
+            "max_iter": 500,
+            "tol": 1e-06,
+            "copy": True
+        },
+        "PLSRegression": {
+            "n_components": 2,
+            "scale": True,
+            "max_iter": 500,
+            "tol": 1e-06,
+            "copy": True
+        },
+        "PLSSVD": {
+            "n_components": 2,
+            "scale": True,
+            "copy": True,
+            "algorithm": "svd"
+        },
+        "FactorAnalysis": {
+            "tol": 1e-2,
+            "copy": True,
+            "max_iter": 1000,
+            "svd_method": "randomized",
+            "iterated_power": 3,
+            "random_state": 0
+        },
+        "FastICA": {
+            "algorithm": "parallel",
+            "whiten": True,
+            "fun": "logcosh",
+            "max_iter": 200,
+            "tol": 1e-4
+        },
+        "IncrementalPCA": {
+            "whiten": False,
+            "copy": True
+        },
+        "MiniBatchSparsePCA": {
+            "alpha": 1,
+            "ridge_alpha": 0.01,
+            "n_iter": 100,
+            "batch_size": 3,
+            "verbose": False,
+            "shuffle": True,
+            "method": "lars",
+            "normalize_components": False
+        },
+        "PCA": {
+            "copy": True,
+            "whiten": False,
+            "svd_solver": "auto",
+            "tol": 1e-3
+        },
+        "KMeans": {
+            "n_clusters": 8,
+            "init": "k-means++",
+            "n_init": 10,
+            "max_iter": 300,
+            "tol": 1e-4,
+            "verbose": 0,
+            "copy_x": True,
+            "algorithm": "auto"
+        },
+        "AgglomerativeClustering": {
+            "n_clusters": 2,
+            "affinity": "euclidean",
+            "linkage": "ward"
+        },
+        "Birch": {
+            "threshold": 0.5,
+            "branching_factor": 50,
+            "n_clusters": 3,
+            "compute_labels": True,
+            "copy": True
+        },
+        "DBSCAN": {
+            "eps": 0.5,
+            "min_samples": 5,
+            "metric": "euclidean",
+            "algorithm": "auto",
+            "leaf_size": 30
+        },
+        "BisectingKMeans": {
+            "n_clusters": 8,
+            "init": "k-means++",
+            "n_init": 1,
+            "max_iter": 300,
+            "tol": 1e-4,
+            "verbose": 0,
+            "copy_x": True,
+            "bisecting_strategy": "biggest_inertia"
+        },
+        "SpectralClustering": {
+            "n_clusters": 8,
+            "n_init": 10,
+            "gamma": 1.0,
+            "affinity": "rbf",
+            "n_neighbors": 10,
+            "assign_labels": "kmeans",
+            "degree": 3,
+            "coef0": 1,
+            "verbose": False
+        },
+        "MiniBatchKMeans": {
+            "n_clusters": 8,
+            "init": "k-means++",
+            "max_iter": 100,
+            "batch_size": 100,
+            "verbose": 0,
+            "compute_labels": True,
+            "tol": 0.0,
+            "max_no_improvement": 10,
+            "n_init": 3,
+            "reassignment_ratio": 0.01
+        },
+        "BaggingRegressor": {
+            "n_estimators": 10,
+            "max_samples": 1.0,
+            "max_features": 1.0,
+            "bootstrap": True,
+            "bootstrap_features": False,
+            "oob_score": False,
+            "warm_start": False,
+            "verbose": 0
+        },
+        "ExtraTreesRegressor": {
+            "n_estimators": 100,
+            "criterion": "mse",
+            "min_samples_split": 2,
+            "min_samples_leaf": 1,
+            "min_weight_fraction_leaf": 0.0,
+            "max_features": "auto",
+            "bootstrap": False,
+            "oob_score": False,
+            "verbose": 0,
+            "warm_start": False
+        },
+        "GradientBoostingRegressor": {
+            "loss": "ls",
+            "learning_rate": 0.1,
+            "n_estimators": 100,
+            "subsample": 1.0,
+            "criterion": "friedman_mse",
+            "min_samples_split": 2,
+            "min_samples_leaf": 1,
+            "min_weight_fraction_leaf": 0.0,
+            "max_depth": 3,
+            "alpha": 0.9,
+            "verbose": 0,
+            "warm_start": False,
+            "validation_fraction": 0.1,
+            "tol": 1e-4
+        },
+        "StackingRegressor": {
+            "verbose": 0,
+            "passthrough": False
+        },
+        "HistGradientBoostingRegressor": {
+            "loss": "squared_error",
+            "learning_rate": 0.1,
+            "max_iter": 100,
+            "max_leaf_nodes": 31,
+            "min_samples_leaf": 20,
+            "l2_regularization": 0.0,
+            "max_bins": 255,
+            "warm_start": False,
+            "early_stopping": "auto",
+            "scoring": "loss",
+            "validation_fraction": 0.1,
+            "n_iter_no_change": 10,
+            "tol": 1e-7
+        },
+        "VotingRegressor": {
+        },
+        "MLPRegressor": {
+            "hidden_layer_sizes": (100,),
+            "activation": "relu",
+            "solver": "adam",
+            "alpha": 0.0001,
+            "batch_size": "auto",
+            "learning_rate": "constant",
+            "learning_rate_init": 0.001,
+            "power_t": 0.5,
+            "max_iter": 200,
+            "shuffle": True,
+            "tol": 1e-4,
+            "verbose": False,
+            "warm_start": False,
+            "momentum": 0.9,
+            "nesterovs_momentum": True,
+            "early_stopping": False,
+            "validation_fraction": 0.1,
+            "beta_1": 0.9,
+            "beta_2": 0.999,
+            "epsilon": 1e-8,
+            "n_iter_no_change": 10
+        },
+        "BernoulliNB": {
+            "alpha": 1.0,
+            "binarize": 0.0,
+            "fit_prior": True
+        },
+        "MLPClassifier": {
+            "hidden_layer_sizes": (100,),
+            "activation": "relu",
+            "solver": "adam",
+            "alpha": 0.0001,
+            "batch_size": "auto",
+            "learning_rate": "constant",
+            "learning_rate_init": 0.001,
+            "power_t": 0.5,
+            "max_iter": 200,
+            "shuffle": True,
+            "tol": 1e-4,
+            "verbose": False,
+            "warm_start": False,
+            "momentum": 0.9,
+            "nesterovs_momentum": True,
+            "early_stopping": False,
+            "validation_fraction": 0.1,
+            "beta_1": 0.9,
+            "beta_2": 0.999,
+            "epsilon": 1e-8,
+            "n_iter_no_change": 10
+        },
+        "BernoulliRBM": {
+            "n_components": 256,
+            "learning_rate": 0.1,
+            "batch_size": 10,
+            "n_iter": 10,
+            "verbose": 0
+        },
+        "CategoricalNB": {
+            "alpha": 1.0,
+            "fit_prior": True
+        },
+        "ComplementNB": {
+            "alpha": 1.0,
+            "fit_prior": True,
+            "norm": False
+        },
+        "GaussianNB": {
+            "var_smoothing": 1e-9
+        },
+        "MultinomialNB": {
+            "alpha": 1.0,
+            "fit_prior": True
+        },
+        "BaggingClassifier": {
+            "n_estimators": 10,
+            "max_samples": 1.0,
+            "max_features": 1.0,
+            "bootstrap": True,
+            "bootstrap_features": False,
+            "oob_score": False,
+            "warm_start": False,
+            "verbose": 0
+        },
+        "ExtraTreesClassifier": {
+            "n_estimators": 100,
+            "criterion": "gini",
+            "min_samples_split": 2,
+            "min_samples_leaf": 1,
+            "min_weight_fraction_leaf": 0.0,
+            "max_features": "auto",
+            "bootstrap": False,
+            "oob_score": False,
+            "verbose": 0,
+            "warm_start": False
+        },
+        "GradientBoostingClassifier": {
+            "loss": "deviance",
+            "learning_rate": 0.1,
+            "n_estimators": 100,
+            "subsample": 1.0,
+            "criterion": "friedman_mse",
+            "min_samples_split": 2,
+            "min_samples_leaf": 1,
+            "min_weight_fraction_leaf": 0.0,
+            "max_depth": 3,
+            "verbose": 0,
+            "warm_start": False,
+            "validation_fraction": 0.1,
+            "tol": 1e-4
+        },
+        "StackingClassifier": {
+            "stack_method": "auto",
+            "verbose": 0,
+            "passthrough": False
+        },
+        "VotingClassifier": {
+            "voting": "hard",
+            "flatten_transform": True
+        },
+        "HistGradientBoostingClassifier": {
+            "loss": "log_loss",
+            "learning_rate": 0.1,
+            "max_iter": 100,
+            "max_leaf_nodes": 31,
+            "min_samples_leaf": 20,
+            "l2_regularization": 0.0,
+            "max_bins": 255,
+            "monotonic_cst": None,
+            "warm_start": False,
+            "early_stopping": "auto",
+            "scoring": "loss",
+            "validation_fraction": 0.1,
+            "n_iter_no_change": 10,
+            "tol": 1e-7
+        },
         "RandomForestRegressor": {
         "n_estimators": 100,
         "max_depth": None,
